cac02d8d96b903a5437d88c125d4f584b927e90e
hnamazian <namazian87@gmail.com>
2019-09-08 15:20:43 +0430

    register login and register page + import core module to auth module

--- src/pages/auth/auth.module.ts
+++ src/pages/auth/auth.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from 'ionic-angular';
+import { CoreModule } from "../core/core.module"
+
+import { LoginPage } from './pages/login/login';
+import { RegisterPage } from './pages/register/register';
 
 @NgModule({
-  imports: [CommonModule, IonicModule],
-  declarations: [],
-  entryComponents: [],
+  imports: [CommonModule, IonicModule, CoreModule],
+  declarations: [LoginPage, RegisterPage],
+  entryComponents: [LoginPage, RegisterPage],
   exports: []
 })
 export class AuthModule {}
