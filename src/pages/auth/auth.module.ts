742c33863168539c9b77f5e5016cb44b5ed753dc
hnamazian <namazian87@gmail.com>
2019-09-08 14:47:52 +0430

    Create auth.module.ts to register authentication component

---
+++ src/pages/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from 'ionic-angular';
+
+@NgModule({
+  imports: [CommonModule, IonicModule],
+  declarations: [],
+  entryComponents: [],
+  exports: []
+})
+export class AuthModule {}
