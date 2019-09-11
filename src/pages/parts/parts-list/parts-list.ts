cd712790f4c798964d46f0eca4f05e4d7eec592b
hnamazian <namazian87@gmail.com>
2019-09-11 11:47:35 +0430

    Create initiall PartsListPage

---
+++ src/pages/parts/parts-list/parts-list.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'login-page',
+  templateUrl: 'login.html'
+})
+export class PartsListPage implements OnInit {
+    
+  constructor() {}
+
+  ngOnInit() {}
+}
