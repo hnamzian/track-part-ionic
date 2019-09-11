f54c8aecc02f256e8f88d5c5c862b87cfc708864
hnamazian <namazian87@gmail.com>
2019-09-11 14:41:58 +0430

    Add template parts list + Filter parts by type at changePartType handler

--- src/pages/parts/parts-list/parts-list.ts
+++ src/pages/parts/parts-list/parts-list.ts
@@ -5,11 +5,52 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'parts-list.html'
 })
 export class PartsListPage implements OnInit {
+  allParts = [
+    {
+      id: '12345678',
+      partName: 'ردیاب',
+      createdAt: '01/12/1398',
+      creator: 'اریاتک',
+      state: 'تست شده',
+      partType: 'BOARD'
+    },
+    {
+      id: '12480312',
+      partName: 'rk-123',
+      createdAt: '01/12/1398',
+      creator: 'اریاتک',
+      state: 'تست شده',
+      partType: 'RAK'
+    }
+  ];
+  parts;
+
   constructor() {}
 
   ngOnInit() {}
 
   changePartType(ev) {
-    console.log(ev.value);
+    this.parts = [];
+    if (ev.value == 'boards') {
+      this.parts = this.allParts.filter(part => {
+        if (part.partType == 'BOARD') return part;
+      });
+    } else if (ev.value == 'raks') {
+      this.parts = this.allParts.filter(part => {
+        if (part.partType == 'RAK') return part;
+      });
+    } else if (ev.value == 'systems') {
+      this.parts = this.allParts.filter(part => {
+        if (part.partType == 'SYSTEM') return part;
+      });
+    } else if (ev.value == 'pcs') {
+      this.parts = this.allParts.filter(part => {
+        if (part.partType == 'PC') return part;
+      });
+    } else if (ev.value == 'antennas') {
+      this.parts = this.allParts.filter(part => {
+        if (part.partType == 'ANTENNA') return part;
+      });
+    }
   }
 }
