7b5c1c88e05a3d060f41993fcc8906c52f4d864c
hnamazian <namazian87@gmail.com>
2019-09-13 11:43:36 +0430

    Implement a qr code scanning template at openScanner

--- src/pages/parts/parts-list/parts-list.ts
+++ src/pages/parts/parts-list/parts-list.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
 
 @Component({
   selector: 'parts-list',
@@ -25,7 +26,7 @@ export class PartsListPage implements OnInit {
   ];
   parts;
 
-  constructor() {}
+  constructor(private qrScanner: QRScanner) {}
 
   ngOnInit() {}
 
@@ -54,5 +55,24 @@ export class PartsListPage implements OnInit {
     }
   }
 
-  openScanner() {}
+  openScanner() {
+    this.qrScanner
+      .prepare()
+      .then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+            console.log('Scanned something', text);
+            this.qrScanner.hide();
+            scanSub.unsubscribe();
+          });
+        } else if (status.denied) {
+          // camera permission was permanently denied
+          // you must use QRScanner.openSettings() method to guide the user to the settings page
+          // then they can grant the permission from there
+        } else {
+          // permission was denied, but not permanently. You can ask for permission again at a later time.
+        }
+      })
+      .catch((e: any) => console.log('Error is', e));
+  }
 }
