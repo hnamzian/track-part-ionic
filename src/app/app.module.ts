28122d2cfd80a2588d4d0d92efd60dc4eb481785
hnamazian <namazian87@gmail.com>
2019-09-14 17:19:46 +0430

    Import HttpClientModule to app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MyApp } from './app.component';
 import { CoreModule } from '../pages/core/core.module';
@@ -16,6 +17,7 @@ import { HomePage } from '../pages/home/home';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpClientModule,
     CoreModule,
     AuthModule,
     UserModule,
