2f73cd384cc4c8c3fcc8367afcb6c9408e460588
hnamazian <namazian87@gmail.com>
2019-09-15 17:59:23 +0430

    Add getPartByType provider function

--- src/providers/parts/parts.ts
+++ src/providers/parts/parts.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { map, catchError } from 'rxjs/operators';
@@ -28,6 +28,34 @@ export class PartsProvider {
 
     return this.http
       .post(url, part, httpOptions)
+      .pipe(
+        catchError((err, caught) => {
+          return err;
+        })
+      )
+      .pipe(
+        map((result: Part) => {
+          return result;
+        })
+      );
+  }
+
+  async getPartByType(partType) {
+    const url = this.baseUrl;
+
+    const authToken = await this.tokenStorage.getAuthToken();
+
+    if (!authToken) return Observable.of({} as Part);
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: authToken
+      }),
+      params: new HttpParams().set('type', partType)
+    };
+
+    return this.http
+      .get(url, httpOptions)
       .pipe(
         catchError((err, caught) => {
           return err;
