7af330ba0e196ce8d56344591263806309c3c226
hnamazian <namazian87@gmail.com>
2019-09-15 17:53:29 +0430

    Add createPart provider function to create new part

--- src/providers/parts/parts.ts
+++ src/providers/parts/parts.ts
@@ -1,13 +1,42 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs/Rx";
-import { map, catchError } from "rxjs/operators";
-import { environment as env } from "../../config/environment.prod";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { map, catchError } from 'rxjs/operators';
+import { environment as env } from '../../config/environment.prod';
+import { TokenStorage } from '../../storage/token';
+import { Part } from '../../models/Part';
+import { Token } from '@angular/compiler';
 
 @Injectable()
 export class PartsProvider {
   baseUrl = `${env.BASE_URL}/parts`;
 
-  constructor(public http: HttpClient) {}
+  constructor(public http: HttpClient, public tokenStorage: TokenStorage) {}
 
+  async createPart(part: Part) {
+    const url = this.baseUrl;
+
+    const authToken = await this.tokenStorage.getAuthToken();
+
+    if (!authToken) return Observable.of({} as Part);
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: authToken
+      })
+    };
+
+    return this.http
+      .post(url, part, httpOptions)
+      .pipe(
+        catchError((err, caught) => {
+          return err;
+        })
+      )
+      .pipe(
+        map((result: Part) => {
+          return result;
+        })
+      );
+  }
 }
