dc849ebae92d66918ec6584b5c3fb16b6d749378
hnamazian <namazian87@gmail.com>
2019-09-15 17:44:11 +0430

    Add getUserProfile() to get profile by auth token

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { map, catchError } from 'rxjs/operators';
 import { environment as env } from '../../config/environment.prod';
+import { TokenStorage } from '../../storage/token';
 import { User } from '../../models/User';
 
 @Injectable()
 export class AuthProvider {
   baseUrl = `${env.BASE_URL}/clients`;
 
-  constructor(public http: HttpClient) {}
+  constructor(public http: HttpClient, public tokenStorage: TokenStorage) {}
 
   async registerUser(user: User) {
     const url = this.baseUrl;
@@ -43,5 +44,28 @@ export class AuthProvider {
           return result;
         })
       );
+  }
+
+  async getUserProfile() {
+    const url = this.baseUrl;
+
+    const authToken = await this.tokenStorage.getAuthToken();
+
+    if (!authToken) return Observable.of({} as User);
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: authToken
+      })
+    };
+
+    return this.http
+      .get(url, httpOptions)
+      .pipe(
+        catchError((err, caught) => {
+          return err;
+        })
+      )
+      .pipe(map((result: User) => result));
   }
 }
