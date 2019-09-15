a90fc8487f25be757e9d2d4853c4b670501b7c5b
hnamazian <namazian87@gmail.com>
2019-09-15 16:40:07 +0430

    Add registerUser() to handle registering new user

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,8 +1,9 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs/Rx";
-import { map, catchError } from "rxjs/operators";
-import { environment as env } from "../../config/environment.prod";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { map, catchError } from 'rxjs/operators';
+import { environment as env } from '../../config/environment.prod';
+import { User } from '../../models/User';
 
 @Injectable()
 export class AuthProvider {
@@ -10,4 +11,20 @@ export class AuthProvider {
 
   constructor(public http: HttpClient) {}
 
+  async registerUser(user: User) {
+    const url = this.baseUrl;
+
+    return this.http
+      .post(url, user)
+      .pipe(
+        catchError((err, caught) => {
+          return err;
+        })
+      )
+      .pipe(
+        map((result: User) => {
+          return result;
+        })
+      );
+  }
 }
