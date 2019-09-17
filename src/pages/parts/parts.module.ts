3a6f33abf1e3a58767818bbd9147d2169988d16d
hnamazian <namazian87@gmail.com>
2019-09-17 17:49:47 +0430

    Register ScannedPartPage to parts.modeule

--- src/pages/parts/parts.module.ts
+++ src/pages/parts/parts.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from 'ionic-angular';
 import { PartsListPage } from './parts-list/parts-list';
+import { ScannedPartPage } from './scanned-part/scanned-part';
 
 @NgModule({
   imports: [CommonModule, IonicModule],
-  declarations: [PartsListPage],
-  entryComponents: [PartsListPage],
+  declarations: [PartsListPage, ScannedPartPage],
+  entryComponents: [PartsListPage, ScannedPartPage],
   exports: []
 })
 export class PartsModule {}
