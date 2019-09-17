8dee2fac0c8afd9385c3995b1e040f4716a8cfbb
hnamazian <namazian87@gmail.com>
2019-09-17 17:49:15 +0430

    Add initial ScannedPartPage

---
+++ src/pages/parts/scanned-part/scanned-part.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'scanned-part',
+  templateUrl: 'scanned-part.html'
+})
+export class ScannedPartPage implements OnInit {
+  constructor() {}
+
+  async ngOnInit() {}
+}
