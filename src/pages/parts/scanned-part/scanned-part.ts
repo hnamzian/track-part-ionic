b109ee47588f0aadcc00f7d6458d516246b4b97c
hnamazian <namazian87@gmail.com>
2019-09-18 11:42:25 +0430

    Get part information sent by parts-list page

--- src/pages/parts/scanned-part/scanned-part.ts
+++ src/pages/parts/scanned-part/scanned-part.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { NavParams } from 'ionic-angular';
+import { Part } from '../../../models/Part';
 
 @Component({
   selector: 'scanned-part',
   templateUrl: 'scanned-part.html'
 })
 export class ScannedPartPage implements OnInit {
-  constructor() {}
+  part = {} as Part;
+
+  constructor(public navParams: NavParams) {
+    this.part = this.navParams.get('part');
+  }
 
   async ngOnInit() {}
 }
