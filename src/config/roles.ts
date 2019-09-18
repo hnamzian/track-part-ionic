2aba93664f0a601fdd0abda5e3e353a5a28b7a1b
hnamazian <namazian87@gmail.com>
2019-09-18 13:56:24 +0430

    Add roles config to translate role names from English to Farsi and vice versa

---
+++ src/config/roles.ts
@@ -0,0 +1,21 @@
+export var roleEnToFa = {
+  BOARD_CREATOR: 'تولید کننده بورد',
+  BOARD_TESTER: 'آزمایش کننده بورد',
+  RAK_CREATOR: 'تولید کننده رک',
+  RAK_TESTER: 'آزمایش کننده رک',
+  SYSTEM_CREATOR: 'تولید کننده سیستم',
+  SYSTEM_TESTER: 'آزمایش کننده سیستم',
+  PC_CONNECTOR: 'نصب کننده رایانه',
+  ANTENNA_CONNECTOR: 'نصب کننده آنتن'
+};
+
+export var roleFaToEn = {
+  'تولید کننده بورد': 'BOARD_CREATOR',
+  'آزمایش کننده بورد': 'BOARD_TESTER',
+  'تولید کننده رک': 'RAK_CREATOR',
+  'آزمایش کننده رک': 'RAK_TESTER',
+  'تولید کننده سیستم': 'SYSTEM_CREATOR',
+  'آزمایش کننده سیستم': 'SYSTEM_TESTER',
+  'نصب کننده رایانه': 'PC_CONNECTOR',
+  'نصب کننده آنتن': 'ANTENNA_CONNECTOR'
+};
