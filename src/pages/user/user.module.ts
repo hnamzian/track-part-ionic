f5ae62352e4e7c5446fcad6949245cee273e5317
hnamazian <namazian87@gmail.com>
2019-09-23 12:40:34 +0330

    Register ProfileQRPage to user.module

--- src/pages/user/user.module.ts
+++ src/pages/user/user.module.ts
@@ -5,10 +5,11 @@ import { CoreModule } from '../core/core.module';
 import { NgxQRCodeModule } from 'ngx-qrcode2';
 import { RegisterProfilePage } from './pages/register-profile/register-profile';
 import { UserProfilePage } from './pages/user-profile/user-profile';
+import { ProfileQRPage } from './pages/profile-qr/profile-qr';
 
 @NgModule({
   imports: [CommonModule, IonicModule, CoreModule, NgxQRCodeModule],
-  declarations: [RegisterProfilePage, UserProfilePage],
-  entryComponents: [RegisterProfilePage, UserProfilePage]
+  declarations: [RegisterProfilePage, UserProfilePage, ProfileQRPage],
+  entryComponents: [RegisterProfilePage, UserProfilePage, ProfileQRPage]
 })
 export class UserModule {}
