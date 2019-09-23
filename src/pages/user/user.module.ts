f5d4ae940d27dc601771276a10e748f81327b933
hnamazian <namazian87@gmail.com>
2019-09-23 12:11:29 +0330

    Register NgxQRCodeModule to user.module

--- src/pages/user/user.module.ts
+++ src/pages/user/user.module.ts
@@ -1,23 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from "ionic-angular";
-import { CoreModule } from "../core/core.module"
-import { RegisterProfilePage } from "./pages/register-profile/register-profile"
-import { UserProfilePage } from "./pages/user-profile/user-profile"
+import { IonicModule } from 'ionic-angular';
+import { CoreModule } from '../core/core.module';
+import { NgxQRCodeModule } from 'ngx-qrcode2';
+import { RegisterProfilePage } from './pages/register-profile/register-profile';
+import { UserProfilePage } from './pages/user-profile/user-profile';
 
 @NgModule({
-  imports: [
-    CommonModule, 
-    IonicModule,
-    CoreModule
-  ],
-  declarations: [
-    RegisterProfilePage,
-    UserProfilePage
-  ],
-  entryComponents: [
-    RegisterProfilePage,
-    UserProfilePage
-  ]
+  imports: [CommonModule, IonicModule, CoreModule, NgxQRCodeModule],
+  declarations: [RegisterProfilePage, UserProfilePage],
+  entryComponents: [RegisterProfilePage, UserProfilePage]
 })
 export class UserModule {}
